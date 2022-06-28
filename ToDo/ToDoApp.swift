461c215973c059784d315e928a7613f3797eef45
Renato Fraga <rofraga74@gmail.com>
2022-06-28 15:16:56 -0500

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Renato Oliveira Fraga on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
